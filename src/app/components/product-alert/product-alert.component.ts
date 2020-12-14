4f5d76f0d41f0d1c0470acaa6436f458dfdd337f
iregraure <iregraure@gmail.com>
2020-12-14 23:56:37 +0100

    Ejercicio terminado

--- src/app/components/product-alert/product-alert.component.ts
+++ src/app/components/product-alert/product-alert.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-product-alert',
@@ -6,6 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-alert.component.css']
 })
 export class ProductAlertComponent implements OnInit {
+
+  @Input() product;
+
+  @Output() notify = new EventEmitter();
 
   constructor() { }
 
